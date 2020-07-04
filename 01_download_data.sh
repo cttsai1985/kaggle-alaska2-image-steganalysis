0574175bc8e66f96a4574acac43f6975a671b543
cttsai1985 <cttsai@outlook.com>
2020-07-03 21:34:10 -0400

    update bash

--- 01_download_data.sh
+++ 01_download_data.sh
@@ -9,4 +9,12 @@ kaggle ${TYPE} download --force -c ${DATA}
 mkdir ${DATA}
 unzip ${DATA} -d ${DATA} 
 
+# external data: clean dift
+TYPE=datasets
+OWNER=cttsai
+DATA=alaska2-image-steganalysis-image-quality
+kaggle ${TYPE} download --force ${OWNER}/${DATA}
+mkdir ${DATA}
+unzip ${DATA} -d ${DATA} 
+
 
