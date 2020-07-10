568f093924bd957d480c06dde3cde35715967ee7
Chia-Ta Tsai <cttsai@outlook.com>
2020-07-10 11:57:48 -0400

    cofigs

--- configs/train_baseline.py
+++ configs/train_baseline.py
@@ -9,11 +9,12 @@ configs = {
     "scheduler_params": {},
 
     "augmentations": [
-        {"transform": "HorizontalFlip", "params": {"p": .5}, },
-        {"transform": "VerticalFlip", "params": {"p": .5}, },
-        {"transform": "RandomRotate90", "params": {"always_apply": False, "p": .5}, },
-        {"transform": "InvertImg", "params": {"p": .5}, },
+        # {"transform": "HorizontalFlip", "params": {"p": .5}, },
+        # {"transform": "VerticalFlip", "params": {"p": .5}, },
+        # {"transform": "RandomRotate90", "params": {"always_apply": False, "p": .5}, },
+        # {"transform": "InvertImg", "params": {"p": .5}, },
         {"transform": "Resize", "params": {"height": 512, "width": 512, "always_apply": True, "p": 1.}, },
+        # {"transform": "ToFloat", "params": {"always_apply": True, "p": 1.}, },
         {"transform": "Normalize", "params": {"always_apply": True, "p": 1.}, },
         {"transform": "ToTensorV2", "params": {"always_apply": True, "p": 1.}, },
     ],
--- configs/train_efficient_net_b2_finetune.py
+++ configs/train_efficient_net_b2_finetune.py
@@ -3,7 +3,7 @@ configs = {
     "batch_size": 14,
 
     "n_epochs": 25,
-    "lr": 2e-07,
+    "lr": 8e-07,
 
     "lr_scheduler": "ReduceLROnPlateau",
     "scheduler_params": {},
@@ -14,8 +14,8 @@ configs = {
         {"transform": "RandomRotate90", "params": {"always_apply": False, "p": .5}, },
         {"transform": "InvertImg", "params": {"p": .5}, },
         {"transform": "Resize", "params": {"height": 512, "width": 512, "always_apply": True, "p": 1.}, },
-        {"transform": "ToFloat", "params": {"always_apply": True, "p": 1.}, },
-        # {"transform": "Normalize", "params": {"always_apply": True, "p": 1.}, },
+        # {"transform": "ToFloat", "params": {"always_apply": True, "p": 1.}, },
+        {"transform": "Normalize", "params": {"always_apply": True, "p": 1.}, },
         {"transform": "ToTensorV2", "params": {"always_apply": True, "p": 1.}, },
     ],
 }
