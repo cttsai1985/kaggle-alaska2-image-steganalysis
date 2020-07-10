b87f2a2346dd36c6c4157da0a1c9a6b31c0dcd48
Chia-Ta Tsai <cttsai@outlook.com>
2020-07-10 09:56:17 -0400

    new configs for efficient

---
+++ configs/test_efficientnet.py
@@ -0,0 +1,36 @@
+configs = {
+    "num_workers": 8,
+    "batch_size": 8,
+
+    "augmentations": [
+#        {"transform": "HorizontalFlip", "params": {"p": .5}, },
+#        {"transform": "VerticalFlip", "params": {"p": .5}, },
+#        {"transform": "RandomRotate90", "params": {"always_apply": False, "p": .5}, },
+#        {"transform": "InvertImg", "params": {"p": .5}, },
+        {"transform": "Resize", "params": {"height": 512, "width": 512, "always_apply": True, "p": 1.}, },
+#        {"transform": "Normalize", "params": {"always_apply": True, "p": 1.}, },
+        {"transform": "ToTensorV2", "params": {"always_apply": True, "p": 1.}, },
+    ],
+
+    "test_time_augmentations": [
+        [
+            {"transform": "Resize", "params": {"height": 512, "width": 512, "always_apply": True, "p": 1.}, },
+#            {"transform": "Normalize", "params": {"always_apply": True, "p": 1.}, },
+            {"transform": "ToTensorV2", "params": {"always_apply": True, "p": 1.}, },
+        ],
+        [
+            {"transform": "HorizontalFlip", "params": {"always_apply": True, "p": 1.}, },
+            # {"transform": "VerticalFlip", "params": {"always_apply": True, "p": 1.}, },
+            {"transform": "Resize", "params": {"height": 512, "width": 512, "always_apply": True, "p": 1.}, },
+#            {"transform": "Normalize", "params": {"always_apply": True, "p": 1.}, },
+            {"transform": "ToTensorV2", "params": {"always_apply": True, "p": 1.}, },
+        ],
+        [
+            {"transform": "VerticalFlip", "params": {"always_apply": True, "p": 1.}, },
+            {"transform": "Resize", "params": {"height": 512, "width": 512, "always_apply": True, "p": 1.}, },
+#            {"transform": "Normalize", "params": {"always_apply": True, "p": 1.}, },
+            {"transform": "ToTensorV2", "params": {"always_apply": True, "p": 1.}, },
+        ],
+    ],
+}
+
